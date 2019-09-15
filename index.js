bbe9d9e2616171a2b3ddfebfdc9019d6544b889a
github username <github email address>
2019-09-14 22:38:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file.
+lowerCaseDrivers = (drivers) => {
+  let lowerCaseDrivers = drivers.map(driver => driver.toLowerCase()); 
+  return lowerCaseDrivers;
+}
+
+//take drivers array and for each string, transform it through map into a hash with first name and last name; shift removes first item of an array, pop removes last item
+function nameToAttributes(drivers){
+  let attributesArr = drivers.map(driver => ({ firstName: driver.split(' ').shift(), lastName: driver.split(' ').pop() }));
+  return attributesArr; 
+}
+
+function attributesToPhrase(drivers){
+  let phrases = drivers.map(driver => (`${driver["name"]} is from ${driver["hometown"]}`))
+  return phrases; 
+}
