cc63c168a84ef0fa983fb78c19e38a9fad59a475
Tracie Masek <42948851+traciemasek@users.noreply.github.com>
2019-09-15 14:25:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file.
+// const lowerCaseDrivers = (drivers) => {
+//   drivers.map = driver => driver.toLowerCase();
+// };
+//these tests don't seem to like arrow functions
+
+function lowerCaseDrivers(drivers) {
+  return drivers.map(function(driver) {
+    return driver.toLowerCase();
+  });
+};
+
+function nameToAttributes(drivers) {
+  return drivers.map(function(driver) {
+    let nameArr = driver.split(" ");
+    let driverNameObj = {};
+    driverNameObj.firstName = nameArr[0];
+    driverNameObj.lastName = nameArr[1];
+    return driverNameObj;
+  });
+};
+
+function attributesToPhrase(drivers) {
+  return drivers.map(function(driver) {
+    return `${driver.name} is from ${driver.hometown}`
+  });
+};
